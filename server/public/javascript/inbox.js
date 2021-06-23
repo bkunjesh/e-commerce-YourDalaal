2930cf236a4f8d7b0b3f2aa3e6173f08eeeb05d0
bkunjesh <bkunjesh@gmail.com>
2021-06-23 09:56:07 +0530

    file structure changes

--- server/public/javascript/inbox.js
+++ server/public/javascript/inbox.js
@@ -1,184 +1,305 @@
 var socket = io.connect();
-const conversationlist=document.querySelector('#conversation-list');
-const chat_list=document.querySelector('#chat-title');
-const chat_message_list=document.querySelector('#chat-message-list');
-const receiverID = document.querySelector('#receiverID').value;
-const send_message = document.querySelector('#send_message');
-var chatID;
-var currentUser;
-var currentReceiver;
-function conversationList()
-{
-    axios.get('/yourdalaal/inbox/getlist',{headers:{"Content-Type" : "application/json"}})
-        .then((res) => {
+const conversationlist = document.querySelector("#conversation-list");
+const chat_list = document.querySelector("#chat-title");
+const chat_message_list = document.querySelector("#chat-message-list");
+const receiverID = document.querySelector("#receiverID").value;
+const send_message = document.querySelector("#send_message");
+const chatID = document.querySelector("#chatID").value;
+const userID = document.querySelector("#userID").value;
 
-            const { user } = res.data;
-            console.log(user)
-            currentUser = user;
-            socket.emit('join', { id: user._id });
+showActiveChat();
+connectSocket();
 
-            user.inbox.sort(function (a, b) {
-                return (a.updatedAt > b.updatedAt) ? -1 : ((a.updatedAt < b.updatedAt) ? 1 : 0);
-            })
-
-            for (let allChats of user.inbox)
-            {
-                const u1_id = allChats.user1._id.toString();
-                const receiver = (u1_id === user._id.toString()) ? allChats.user2 : allChats.user1;
-
-                var updatedDate = getUpdatedDate(allChats.updatedAt);
-                if (allChats.messages.length > 0)
-                    var recent_message = allChats.messages[allChats.messages.length - 1].body;
-                else var recent_message = "";
-                const userProfileImage = (receiver.profileImage) ? receiver.profileImage.url : 'https:img.icons8.com/metro/26/000000/user-male.png';
-                conversationlist.innerHTML +=
-                    `<div style="{text-decoration: none}">
-                        <a href="/yourdalaal/inbox/${receiver._id}" >
-                            <div class="conversation">
-                                <img src=${userProfileImage} />
-                                <div class="title-text">${receiver.username}</div>
-                                <div class="created-date">${updatedDate}</div>
-                                <div class="conversation-message">
-                                    ${recent_message}
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    `
-                
-                if (receiver._id.toString() === receiverID)
-                {
-                    currentReceiver = receiver;
-                    chatID = allChats._id;
-                    chat_list.innerHTML +=
-                    `
-                        <span>${receiver.username}</span>
-                    `
-                    const messagesize = (allChats.messages.length)
-                    for (var i = Math.max(0, messagesize - 100); i < messagesize;i++)
-                    {
-                        const message = allChats.messages[i];
-                        if (message.sender.toString() === receiver._id.toString())
-                        {
-                            chat_message_list.innerHTML +=
-                            `
-                                <div class="message-row other-message">
-                                    <div class="message-content">
-                                        <img src=${userProfileImage} alt="${receiver.username}" />
-                                        <div class="message-text">
-                                            ${message.body}
-                                        </div>
-                                        <div class="message-time"></div>
-                                    </div>
-                                </div>   
-                            `
-                        }
-                        else
-                        {
-                            chat_message_list.innerHTML +=
-                            `
-                                <div class="message-row you-message">
-                                    <div class="message-content">
-                                        <div class="message-text">${message.body}</div>
-                                        <div class="message-time"></div>
-                                    </div>
-                                </div>    
-                            `
-                        }
-                    }
-                    scrollDown();
-                }
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+function connectSocket() {
+    socket.emit("join", { id: userID });
 }
 
-conversationList();
+if (send_message) {
+    send_message.addEventListener("click", async function () {
+        const message_text = document.querySelector("#message-text").value;
+        if (receiverID != "" && isvalidmessage(message_text)) {
+            // let date=new Date();
+            let data = {
+                message_text: message_text,
+                receiverID: receiverID,
+                chatID: chatID,
+            };
 
+            axios
+                .post(`/yourdalaal/inbox/sendMessage`, data, {
+                    headers: { "Content-Type": "application/json" },
+                })
+                .then((res) => {
+                    let date_ = new Date();
+                    let { date, time } = getUpdatedDate(date_);
+                    message_id = res.data.message_id;
+                    // console.log(res.data);
+                    checkForLatestDateUpdate(date);
 
-if (send_message)
-{
-    send_message.addEventListener("click",async function () {
-        const message_text = document.querySelector('#message-text').value;
-        if (receiverID!=''&&isvalidmessage(message_text))
-        {
-            let data = { message_text: message_text, receiverID: receiverID, chatID:chatID };
-            
-            axios.post(`/yourdalaal/inbox/sendMessage`,data,{headers:{"Content-Type" : "application/json"}})
-            .then((res) => {
-                
-                chat_message_list.innerHTML +=
-                `
-                    <div class="message-row you-message">
-                        <div class="message-content">
-                            <div class="message-text">${message_text}</div>
-                            <div class="message-time"></div>
+                    chat_message_list.innerHTML += `
+                <div class="message-row you-message" data-message-id="${message_id}">
+                    <div class="message-content">
+                        <div class="message-text">${message_text}</div>
+                        <div class="message-status">
+                                <div class="message-time">${time}</div>
+                                <div class="read-reciept"></div>
                         </div>
-                    </div>    
-                `
-                scrollDown();
-                document.querySelector('#message-text').value = '';
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-            
+                    </div>
+                </div>    
+            `;
+                    scrollDown();
+                    document.querySelector("#message-text").value = "";
+
+                    conversations = document.querySelectorAll(".conversation");
+                    var node;
+                    conversations.forEach((element) => {
+                        if (element.dataset.receiverId == receiverID) {
+                            recentMessage = element.querySelector(
+                                ".conversation-message"
+                            );
+                            messegeDate =
+                                element.querySelector(".created-date");
+                            node = element;
+                            recentMessage.textContent = message_text;
+                            messegeDate.textContent = date;
+                        }
+                    });
+                    if (node) {
+                        updateConversationList(node);
+                    }
+                })
+                .catch((err) => {
+                    console.log("error while sending message", err);
+                });
+        }
+    });
+    var input = document.querySelector("#message-text");
+    input.addEventListener("keyup", function (event) {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.ctrlKey && event.keyCode === 13) {
+            event.preventDefault();
+            send_message.click();
         }
     });
 }
 
+socket.on("message", function (data) {
+    if (receiverID === data.senderId.toString()) {
+        readAcknowledge = {
+            chatID: chatID,
+            message_id: data.message_id,
+            receiverID: receiverID,
+        };
+        socket.emit("readAcknowledge", readAcknowledge);
 
-socket.on('message', function (data) {
-    if (receiverID === data.senderID.toString()) {
-        const userProfileImage = (currentReceiver&&currentReceiver.profileImage) ? currentReceiver.profileImage.url : 'https:img.icons8.com/metro/26/000000/user-male.png';
-        chat_message_list.innerHTML +=
+        checkForLatestDateUpdate(data.message_date);
+
+        chat_message_list.innerHTML += `
+            <div class="message-row other-message" data-message-id="${data.message_id}">
+                <div class="message-content">
+                    <img src=${data.profileImage} />
+                    <div class="message-text">
+                        ${data.message_text}
+                    </div>
+                    <div class="message-time">${data.message_time}</div>
+                </div>
+            </div>  
+        `;
+    }
+    messageArrived(data);
+
+    scrollDown();
+});
+
+socket.on("readAcknowledge", (data) => {
+    let messages = document.querySelectorAll(`[data-message-id]`);
+    messages.forEach((message) => {
+        if (message.dataset.messageId == data.message_id) {
+            let readReceipt = message
+                .querySelector(".message-content")
+                .querySelector(".message-status")
+                .querySelector(".read-reciept");
+            if (readReceipt) {
+                readReceipt.innerHTML = "&#10003";
+                playTickSound();
+            }
+        }
+    });
+});
+socket.on("readAllAcknowledge", (data) => {
+    if (chatID == data.chatID) {
+        readReceipts = document.querySelectorAll(".read-reciept");
+        for (let i = readReceipts.length - 1; i >= 0; i--) {
+            if (chatID == data.chatID && readReceipts[i].innerHTML == "") {
+                readReceipts[i].innerHTML = "&#10003";
+                playTickSound();
+            } else i = 0;
+        }
+    }
+});
+
+function checkForLatestDateUpdate(message_date) {
+    chat_message_list_date = document.querySelectorAll(
+        "#chat-message-list-date"
+    );
+    if (chat_message_list_date) {
+        var lastChatDate =
+            chat_message_list_date[chat_message_list_date.length - 1]
+                .childNodes[1];
+    }
+    if (!chat_message_list_date || lastChatDate.innerText != message_date) {
+        chat_message_list.innerHTML += `
+                <div id="chat-message-list-date">
+                            <span>
+                                ${message_date}
+                            </span>
+                </div>
+                `;
+    }
+}
+
+function messageArrived(data) {
+    conversations = document.querySelectorAll(".conversation");
+    var node;
+    conversations.forEach((element) => {
+        recentMessage = element.querySelector(".conversation-message");
+
+        if (element.dataset.receiverId == data.senderId.toString()) {
+            node = element;
+            recentMessage.textContent = data.message_text;
+            unread = element.querySelector(".unReadCount-message");
+            if (unread && receiverID != data.senderId.toString()) {
+                unread.textContent = parseInt(unread.textContent) + 1;
+            } else if (receiverID != data.senderId.toString()) {
+                element.innerHTML += `
+                        <div class="unReadCount-message">1</div>
+                    `;
+            }
+        }
+    });
+    if (node) {
+        updateConversationList(node);
+    } else {
+        conversationlist.innerHTML =
             `
-                <div class="message-row other-message">
-                    <div class="message-content">
-                        <img src=${userProfileImage} />
-                        <div class="message-text">
+                <a href="/yourdalaal/inbox/${data.senderId}" data-receiver-id="${data.senderId}">
+                    <div class="conversation" data-receiver-id="${data.senderId}">
+                        <img src='${data.profileImage}'/>
+                        <div class="title-text">${data.senderUsername}</div>
+                        <div class="created-date">${data.message_date}</div>
+                        <div class="conversation-message">
                             ${data.message_text}
                         </div>
-                        <div class="message-time"></div>
+                        <div class="unReadCount-message">1</div>
                     </div>
-                </div>  
-            `
+                    </a>
+                ` + conversationlist.innerHTML;
     }
-    scrollDown();
-})
-            
-            
+}
+
+function updateConversationList(node) {
+    if (node) {
+        node = node.parentNode;
+        parent = node.parentNode;
+        parent.removeChild(node);
+        parent.insertBefore(node, parent.firstChild);
+    }
+}
+
+socket.on("online", function (data) {
+    if (data.connecteduser[receiverID]) {
+        const online_status_text = document.querySelector(
+            "#online-status-text"
+        );
+        if (online_status_text) {
+            online_status_text.innerHTML = " &#128994 online";
+        }
+    }
+    conversations = document.querySelectorAll(".conversation");
+    var node;
+    conversations.forEach((element) => {
+        if (data.connecteduser[element.dataset.receiverId]) {
+            titleText = element
+                .querySelector(".title-text")
+                .querySelector(".title-text-status");
+
+            titleText.style.display = "inline";
+        }
+    });
+    // console.log(data.connecteduser[receiverID]);
+});
+socket.on("offline", function (data) {
+    if (!data.connecteduser[receiverID]) {
+        const online_status_text = document.querySelector(
+            "#online-status-text"
+        );
+        if (online_status_text) {
+            online_status_text.innerHTML = "offline";
+        }
+    }
+    conversations = document.querySelectorAll(".conversation");
+    var node;
+    conversations.forEach((element) => {
+        if (!data.connecteduser[element.dataset.receiverId]) {
+            titleText = element
+                .querySelector(".title-text")
+                .querySelector(".title-text-status");
+
+            titleText.style.display = "none";
+        }
+    });
+});
+
+function getUpdatedDate(date) {
+    //https://stackoverflow.com/questions/27012854/change-iso-date-string-to-date-object-javascript
+    function isoFormatDMY(d) {
+        function pad(n) {
+            return (n < 10 ? "0" : "") + n;
+        }
+        return (
+            pad(d.getDate()) +
+            "/" +
+            pad(d.getMonth() + 1) +
+            "/" +
+            d.getFullYear()
+        );
+    }
+    function getTime(date) {
+        let time = date.toLocaleTimeString("en-US");
+        var b = time.split(":");
+        var c = b[2].split(" ");
+        return b[0] + ":" + b[1] + ":" + c[1];
+    }
+    time = getTime(date);
+    date = isoFormatDMY(date);
+    return { date, time };
+}
+
+scrollDown();
 function scrollDown() {
+    const chat_message_list = document.querySelector("#chat-message-list");
     chat_message_list.scrollTop = chat_message_list.scrollHeight;
 }
-function isvalidmessage(message_text)
-{
-    if(message_text.length>0)
-    for (let ch of message_text)
-    {
-        if (ch != ' ') return 1;
-    }
+function isvalidmessage(message_text) {
+    if (message_text.length > 0)
+        for (let ch of message_text) {
+            if (ch != " ") return 1;
+        }
     return 0;
 }
-
-function getUpdatedDate(date)
-{
-    //https://stackoverflow.com/questions/27012854/change-iso-date-string-to-date-object-javascript
-
-    function parseISOString(s) {
-        var b = s.split(/\D+/);
-        return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+function showActiveChat() {
+    if (receiverID) {
+        conversations = document.querySelectorAll(".conversation");
+        conversations.forEach((element) => {
+            if (element.dataset.receiverId == receiverID.toString()) {
+                element.classList.add("active");
+            }
+        });
     }
-
-    function isoFormatDMY(d) {  
-        function pad(n) {return (n<10? '0' :  '') + n}
-        return pad(d.getUTCDate()) + '/' + pad(d.getUTCMonth() + 1) + '/' + d.getUTCFullYear();
-    }
-
-    date_ = parseISOString(date);
-    return isoFormatDMY(date_);
 }
-
-
+function playTickSound() {
+    // https://stackoverflow.com/questions/15483455/play-sound-when-message-received
+    var audio = new Audio("/media/tick.mp3");
+    audio.volume = 0.1;
+    audio.play();
+}
